de849c23ebcf37199fa3a8ecb2cd8120e5487a79
neocydfr <franckboudraa@gmail.com>
2018-02-14 10:00:34 +0100

    fix mail port bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://movegroove.herokuapp.com', port: 80 }
+  config.action_mailer.default_url_options = { host: 'https://movegroove.herokuapp.com'}
+  #config.mailer_sender = '"Franck" <franckboudraa@gmail.com>'
 end
