55a1ab49f495caef36847929c2abb907e189db15
neocydfr <franckboudraa@gmail.com>
2018-02-18 17:31:59 +0100

    adding testimonials on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :activities
+
   devise_for :users, controllers: {
       sessions: 'users/sessions'
   }
+
   resources :users, only: [:show]
 
   root to: "static_pages#homepage"
