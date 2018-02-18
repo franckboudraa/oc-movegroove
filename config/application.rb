3d7199c0270073a69fa22560d3ea6a239e96f85e
neocydfr <franckboudraa@gmail.com>
2018-02-18 15:54:05 +0100

    fixed display issues on register page

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module MoveGroove
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+    html_tag
+    }
   end
 end
